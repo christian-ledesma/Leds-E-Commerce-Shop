7d53d2c2489ab4aa50a68ce7dcbd9c2235d9e97e
Christian <christian_ledesma_a@hotmail.com>
2023-01-20 01:16:19 +0100

    Add docker compose and finished implementations on Catalog API Add Basket API project and implemented CRUD with Redis

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice
+        {
+            get
+            {
+                var totalPrice = 0m;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
