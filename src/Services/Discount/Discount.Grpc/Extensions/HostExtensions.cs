82bc6be31d280f73ee878362c3424c819d2a9132
Christian <christian_ledesma_a@hotmail.com>
2023-02-27 22:34:14 +0100

    Add Discount.Grpc project to solution

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Npgsql;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static WebApplicationBuilder MigrateDatabase<T>(this WebApplicationBuilder builder, int? retry = 0)
+        {
+            var retryForAvailability = retry.Value;
+
+            var configuration = builder.Configuration;
+
+            try
+            {
+                using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+
+                using var command = new NpgsqlCommand
+                {
+                    Connection = connection
+                };
+
+                command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                command.ExecuteNonQuery();
+
+                command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, 
+                                                            ProductName VARCHAR(24) NOT NULL,
+                                                            Description TEXT,
+                                                            Amount INT)";
+                command.ExecuteNonQuery();
+
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                command.ExecuteNonQuery();
+            }
+            catch (NpgsqlException)
+            {
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    Thread.Sleep(2000);
+                    MigrateDatabase<T>(builder, retryForAvailability);
+                }
+            }
+
+            return builder;
+        }
+    }
+}
