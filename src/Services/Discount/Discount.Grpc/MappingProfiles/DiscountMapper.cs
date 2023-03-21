67e96849beb117bdf4e11f229f56b9734d75b0b1
Christian Ledesma <christian.ledesma@copernicus.es>
2023-03-21 16:41:28 +0100

    Add DiscountService to the Discount.Grpc project

---
+++ src/Services/Discount/Discount.Grpc/MappingProfiles/DiscountMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.MappingProfiles
+{
+    public class DiscountMapper : Profile
+    {
+        public DiscountMapper()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
