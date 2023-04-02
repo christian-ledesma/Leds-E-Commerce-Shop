b6b235a646c9e5022a901c649b93def0f237ce19
Christian Ledesma <christian.ledesma@copernicus.es>
2023-04-02 18:12:37 +0200

    Add Order API Project with Clean Architecture - Also added some entities and common classes

---
+++ src/Services/Order/Order.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.Application.Contracts.Persistence
+{
+    using Order.Domain.Entities;
+
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/Services/Order/Order.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace Order.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+    }
+}
