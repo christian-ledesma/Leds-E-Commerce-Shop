b9dc5c0fba807bc6b83f928bf1fea01f468fa3d3
Christian Ledesma <christian.ledesma@copernicus.es>
2023-04-04 15:37:32 +0200

    Add repositories

---
+++ src/Services/Order/Order.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Order.Application.Contracts.Persistence;
+using Order.Infrastructure.Persistence;
+
+namespace Order.Infrastructure.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using Order.Domain.Entities;
+
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orders = await _context.Orders
+                                .Where(o => o.UserName == userName)
+                                .ToListAsync();
+            return orders;
+        }
+    }
+}
